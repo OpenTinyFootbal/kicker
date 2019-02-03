7e1d0e4b4cfa7bbcaf737984dd49bf6845371e3f
Damien Bouvy <dbo@odoo.com>
2019-02-03 15:36:27 +0100

    [IMP] kicker: indices & stuff

--- kicker/models/kicker_game.py
+++ kicker/models/kicker_game.py
@@ -9,7 +9,7 @@ class KickerGame(models.Model):
     _order = 'create_date DESC'
 
     create_date = fields.Datetime(default=fields.Datetime.now)
-    kicker_id = fields.Many2one('kicker.kicker', string='Kicker')
+    kicker_id = fields.Many2one('kicker.kicker', string='Kicker', ondelete='restrict', index=True)
     winning_team = fields.Selection([('team_1', 'Team 1'), ('team_2', 'Team 2')], compute='_compute_winning_team', store=True)
     score_1 = fields.Integer(string="Team 1 Score", required=True)
     score_2 = fields.Integer(string="Team 2 Score", required=True)
@@ -24,10 +24,10 @@ class KickerSession(models.Model):
     _name = 'kicker.session'
     _description = 'Kicker Session'
 
-    game_id = fields.Many2one('kicker.game', required=True)
+    game_id = fields.Many2one('kicker.game', required=True, index=True)
     won = fields.Boolean(compute='_compute_won', store=True)
     team = fields.Selection([('team_1', 'Team 1'), ('team_2', 'Team 2')], required=True)
-    player_id = fields.Many2one('res.partner', string='Player')
+    player_id = fields.Many2one('res.partner', string='Player', index=True)
     game_date = fields.Datetime(related='game_id.create_date', store=True)
 
     @api.depends('game_id', 'game_id.winning_team')
