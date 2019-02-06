2536ed4efce2b7469cbb45cadaa790aa97ce9cf0
Damien Bouvy <dbo@odoo.com>
2019-02-06 09:18:05 +0100

    [IMP] kicker: better demo data
    remove me in stable ;)

--- kicker/models/kicker_game.py
+++ kicker/models/kicker_game.py
@@ -23,6 +23,40 @@ class KickerGame(models.Model):
     def _compute_winning_team(self):
         for game in self:
             game.winning_team = 'team_1' if game.score_1 > game.score_2 else 'team_2'
+    
+    @api.depends('score_1', 'score_2', 'session_ids')
+    def _compute_name(self):
+        for game in self:
+            game.name = '@'.join([str(game.date), game.kicker_id.name])
+    
+#    @api.constrains('session_ids')
+#    def _validate_session(self):
+#        for game in self:
+#            if len(game.session_ids != 4):
+#                raise ValidationError(_('There must be 4 players per game'))
+
+    @api.model
+    def _generate_demo_data(self, amount=100):
+        seconds_in_year = 365*24*60*60
+        vals = list()
+        kickers = self.env['kicker.kicker'].search([])
+        for i in range(amount):
+            players = self.env['res.partner'].search([('kicker_player', '=', True)])
+            player_ids = random.sample(players.ids, 4)
+            date = datetime.datetime.now() - datetime.timedelta(seconds=random.randrange(0,seconds_in_year))
+            vals.append({
+                'date': date,
+                'kicker_id': kickers[random.randrange(0, len(kickers))].id,
+                'score_1': 11,
+                'score_2': 11 - random.randrange(2, 11),
+                'session_ids': [
+                    (0, False, {'team': 'team_1', 'player_id': player_ids[1]}),
+                    (0, False, {'team': 'team_2', 'player_id': player_ids[2]}),
+                    (0, False, {'team': 'team_2', 'player_id': player_ids[3]}),
+                    (0, False, {'team': 'team_1', 'player_id': player_ids[0]}),
+                ]
+            })
+        self.create(vals)
 
 class KickerSession(models.Model):
     _name = 'kicker.session'
